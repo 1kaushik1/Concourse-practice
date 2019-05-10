bc0c2b61db6e0d872001a4c62acbf786cbe080eb
root <root@ip-10-1-200-194.eu-west-1.compute.internal>
2019-05-10 13:20:14 +0000

    change3

--- tutorials/basic/task-scripts/task_show_uname.sh
+++ tutorials/basic/task-scripts/task_show_uname.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
 uname -a
-ls ../.
-pwd
-ls ./.
-cd resource-tutorials
-pwd
-ls
-cd resource-tutorials/tutorials/basic/task-scripts
-ls
 sh resource-tutorials/tutorials/basic/task-scripts/task_show_uname-2.sh
